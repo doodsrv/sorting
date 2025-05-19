c2c99ecc9889e7fe66df0e65559739a8455fd7ba
doodsrv <142223452+doodsrv@users.noreply.github.com>
2025-05-19 14:40:56 +0700

    Add files via upload
    program selection sort, quicksort, dan merge sort

---
+++ quicksort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {33, 21, 45, 64, 55, 34};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "Before sorting: ";
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "After sorting: ";
+    printArray(arr, n);
+
+    return 0;
+}
+
